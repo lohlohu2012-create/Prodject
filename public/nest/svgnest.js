627ab2cf1a62eb01c5cb41084d4ecd9f61fbb335
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-21 09:33:43 +0300

    Add benchmark switch for persistent NFP cache

--- public/nest/svgnest.js
+++ public/nest/svgnest.js
@@ -33,7 +33,8 @@
 			populationSize: 10,
 			mutationRate: 10,
 			useHoles: false,
-			exploreConcave: false
+			exploreConcave: false,
+			persistNfpCache: true
 		};
 		
 		this.working = false;
@@ -145,6 +146,9 @@
 			
 			if('exploreConcave' in c){
 				config.exploreConcave = !!c.exploreConcave;
+			}
+			if('persistNfpCache' in c){
+				config.persistNfpCache = !!c.persistNfpCache;
 			}
 			
 			SvgParser.config({ tolerance: config.curveTolerance});
@@ -332,12 +336,15 @@
 			
 			var nfpPairs = [];
 			var key;
+			var newCache = {};
 			
 			for(i=0; i<placelist.length; i++){
 				var part = placelist[i];
 				key = {A: binPolygon.id, B: part.id, inside: true, Arotation: 0, Brotation: rotations[i]};
 				if(!nfpCache[JSON.stringify(key)]){
 					nfpPairs.push({A: binPolygon, B: part, key: key});
+				}else{
+					newCache[JSON.stringify(key)] = nfpCache[JSON.stringify(key)];
 				}
 				for(j=0; j<i; j++){
 					var placed = placelist[j];
