f1545ca18d00e2f7e1e3d1e15ffe6c4d88e4e885
lohlohu2012-create <loh.lohu2012@gmail.com>
2026-09-21 10:19:55 +0300

    Isolate stale nesting workers and propagate engine errors

--- public/nest/svgnest.js
+++ public/nest/svgnest.js
@@ -49,6 +49,7 @@
 		var progress = 0;
 		var displayCounter = 0;
 		var lastDisplayTime = 0;
+		var engineSession = 0;
 		
 		this.parsesvg = function(svgstring){
 			// reset if in progress
@@ -179,7 +180,8 @@
 		
 		// progressCallback is called when progress is made
 		// displayCallback is called when a new placement has been made
-		this.start = function(progressCallback, displayCallback){						
+		this.start = function(progressCallback, displayCallback, errorCallback){
+			var sessionId = ++engineSession;						
 			if(!svg || !bin){
 				return false;
 			}
@@ -282,16 +284,17 @@
 			this.working = false;
 			
 			workerTimer = setInterval(function(){
+				if(sessionId !== engineSession)return;
 				if(!self.working){
-					self.launchWorkers.call(self, tree, binPolygon, config, progressCallback, displayCallback);
 					self.working = true;
+					self.launchWorkers.call(self, tree, binPolygon, config, progressCallback, displayCallback, errorCallback, sessionId);
 				}
 				
 				progressCallback(progress);
 			}, 100);
 		}
 		
-		this.launchWorkers = function(tree, binPolygon, config, progressCallback, displayCallback){
+		this.launchWorkers = function(tree, binPolygon, config, progressCallback, displayCallback, errorCallback, sessionId){
 			function shuffle(array) {
 			  var currentIndex = array.length, temporaryValue, randomIndex ;
 
@@ -593,7 +596,9 @@
 				p2.require('placementworker.js');				
 				
 				p2.map(worker.placePaths).then(function(placements){
+					if(sessionId !== engineSession)return;
 					if(!placements || placements.length == 0){
+						self.working = false;
 						return;
 					}
 					
@@ -627,16 +632,21 @@
 
 					var now = Date.now();
 					var shouldDisplay = isBest || (now - lastDisplayTime >= 220);
+					if(sessionId !== engineSession)return;
 					if(shouldDisplay && typeof displayCallback === 'function'){
 						lastDisplayTime = now;
 						displayCallback(self.applyPlacement(bestresult.placements), placedArea/totalArea, numPlacedParts, numParts, isBest, ++displayCounter);
 					}
 					self.working = false;
 				}, function (err) {
-					console.log(err);
+					if(sessionId !== engineSession)return;
+					self.working = false;
+					if(typeof errorCallback === 'function')errorCallback(err);
 				});
 			}, function (err) {
-				console.log(err);
+				if(sessionId !== engineSession)return;
+				self.working = false;
+				if(typeof errorCallback === 'function')errorCallback(err);
 			});
 		}
 		
@@ -890,9 +900,11 @@
 		}
 		
 		this.stop = function(){
+			engineSession++;
 			this.working = false;
 			if(workerTimer){
 				clearInterval(workerTimer);
+				workerTimer = null;
 			}
 		};
 	}
